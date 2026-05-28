5984d4e6a4ceeb2e15441a2033b0e2791a000c94
Shrayas Sarker <sarkershrayas@gmail.com>
2026-05-28 23:33:15 +0600

    adding projects

---
+++ Unity_Jr_Programer_Projects/Counting Prototype/Assets/Scripts/BoxMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BoxMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private float horizontalInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
