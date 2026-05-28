316a38cd14b483cb8d167fa0fe269a2c87831098
Shrayas Sarker <sarkershrayas@gmail.com>
2026-05-28 23:39:01 +0600

    Add ModTheCube Unity project files
    Initial import of the ModTheCube Unity project. Adds Input System actions asset, Cube.cs (positions, scales and rotates the cube and sets a translucent red material), Cube material, scenes, tutorial/readme assets, project settings and package manifest to set up the project.

---
+++ Unity_Jr_Programer_Projects/ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float rotationSpeed = 20f;
+
+    void Start()
+    {
+        transform.position = new Vector3(1, 1, 1);
+        transform.localScale = new Vector3(2f, 2f, 2f);
+
+        Material material = Renderer.material;
+
+        material.color = new Color(1f, 0f, 0f, 0.5f);// R G B A 
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0.0f, rotationSpeed* Time.deltaTime, 0.0f);
+    }
+    
+}
